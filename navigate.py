a1b465cd733706b565672b7832364d179ba3f33d
Juan Pablo Rios Escobar <jprioses@outlook.com>
2022-09-26 12:33:15 -0500

    Separated added and commited all files

---
+++ navigate.py
@@ -0,0 +1,114 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+#Start Chrome web browser 
+# chromeBrowser = webdriver.Chrome(service=Service("./chromedriver.exe"))
+chromeBrowser = webdriver.Chrome()
+
+#set the country, league and season, it may be equal to web page link
+country = 'england'
+league = 'premier-league'
+season = '2021-2022'
+leagueLink = 'https://www.livesport.com/en/soccer/'+ country + '/' + league + '-' + season + '/results/'
+
+def openUrl(url):
+    chromeBrowser.get(url)
+
+def acceptCookies():
+        try:
+            buttonAccept = WebDriverWait(chromeBrowser, 2).until(EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler')))
+            buttonAccept.click()
+        except:
+            print('Couldnt reach button')
+
+def showMoreMatches():
+    arrayAllMatches = []
+    while True:
+        try:
+            moreMatches = chromeBrowser.find_element(By.LINK_TEXT,'Show more matches') 
+            ActionChains(chromeBrowser).scroll_to_element(moreMatches).perform()
+            loading = chromeBrowser.find_element(By.CLASS_NAME, 'loadingAnimation').text
+
+            while (loading=='LOADING...'):
+                loading = chromeBrowser.find_element(By.CLASS_NAME, 'loadingAnimation').text
+            
+            moreMatches.click()
+            print('Clicked')
+        except :
+            arrayAllMatches = chromeBrowser.find_elements(By.XPATH, "//*[starts-with(@id, 'g_1_')]") 
+            print('Couldnt find click button')
+            return arrayAllMatches
+
+def eachMatch(match):
+    ActionChains(chromeBrowser).scroll_to_element(match).perform()
+    match.click()
+    chromeBrowser.switch_to.window(chromeBrowser.window_handles[1])
+    acceptCookies()
+
+def eachMatchInfo():
+
+    dateTime = chromeBrowser.find_element(By.CLASS_NAME, 'duelParticipant__startTime').text
+    
+    roundContainer = chromeBrowser.find_element(By.CLASS_NAME, 'tournamentHeader__country') 
+    roundInfo = roundContainer.find_element(By.TAG_NAME, 'a').text
+    
+    return dateTime, roundInfo
+
+def eachMatchGoals(team):
+    name = chromeBrowser.find_element(By.CLASS_NAME, 'duelParticipant__' + team).text
+    scoreContainer = chromeBrowser.find_element(By.CLASS_NAME, 'detailScore__wrapper')
+    scoreArray = scoreContainer.find_elements(By.TAG_NAME, 'span') 
+
+    if (team=='home'):
+        score = int(scoreArray[0])
+    if (team=='away'):
+        score = int(scoreArray[2])
+
+    goalData = chromeBrowser.find_element(By.CLASS_NAME, 'smv__verticalSections' )
+            
+    goalContainer = goalData.find_elements(By.CLASS_NAME, 'smv__' + team + 'Participant')
+
+    goals = []   
+    scorer = [] 
+
+    for data in goalContainer:
+        try:
+            data.find_element(By.CLASS_NAME,'soccer')
+            minuteString = data.find_element(By.CLASS_NAME, 'smv__timeBox').text
+            minute = minuteString.replace("'","",1)
+            goals.append(minute)
+            name = data.find_element(By.CLASS_NAME, 'smv__playerName').text
+            scorer.append(name)
+        except:
+            print('Not a Goal')
+
+    return name, score, goals, scorer
+
+def eachMatchStats(team, half, isFirstClick):
+
+    if (isFirstClick):
+        chromeBrowser.find_element(By.LINK_TEXT, 'STATISTICS').click()
+
+    WebDriverWait(chromeBrowser, 2).until(EC.presence_of_element_located((By.LINK_TEXT, half))).click()
+    statsArray = chromeBrowser.find_elements(By.CLASS_NAME, 'stat__category')
+    statsObject = {}
+    for stat in statsArray:
+        try:
+            key = stat.find_element(By.CLASS_NAME, 'stat__categoryName').text
+            value = stat.find_element(By.CLASS_NAME, 'stat__' + team + 'Value').text
+            statsObject[key] = value
+        except:
+            print('No such Element')
+
+    return statsObject
+
+def closeWindow():
+    chromeBrowser.close()
+    chromeBrowser.switch_to.window(chromeBrowser.window_handles[0])
+
+
+    
